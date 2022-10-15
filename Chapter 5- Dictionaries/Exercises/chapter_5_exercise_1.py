cf4599a04388b2794a91dddcf1b094e89538ee86
jasfer john <jasferjohndejesus@yahoo.com>
2022-10-15 16:23:20 +0400

    1

---
+++ Chapter 5- Dictionaries/Exercises/chapter_5_exercise_1.py
@@ -0,0 +1,11 @@
+friend = {
+    'first_name': 'Felix',
+    'last_name' : 'Lengyel',
+    'age' : 26,
+    'city' : "Quebec"
+}
+
+print(friend['first_name'])
+print(friend['last_name'])
+print(friend['age'])
+print(friend['city'])
